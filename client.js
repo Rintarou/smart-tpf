44c5c29bf3ad055f124d1ec2a7644e6ccba0e135
Rintarou <merlin.barzilai@gmail.com>
2018-02-22 23:40:29 +0100

    Client interface start (query class)

---
+++ client.js
@@ -0,0 +1,40 @@
+var client = angular.module("client", []);
+
+client.controller("clientController", function($scope, $http)
+{
+  var Query = function(raw) {
+    var SparqlParser = require('sparqljs').Parser;
+    var parser = new SparqlParser();
+
+    this.raw = parser.parse(raw);
+    this.subQueries = {};
+    this.results = {};
+
+    this.optimize = function () {
+      //TODO: actually slice the query in subqueries and
+    }
+
+    this.send = function () {
+      if(this.subQueries == {}) {
+        throw new Error("query needs to be subqueried and optimized");
+      }
+      //using angular?
+      for (var subquery in subQueries) {
+        var stop = false;
+        this.results[subquery] =  {};
+        var slice = 0;
+        while(!stop) {
+          $http.post("client.serverURL //TODO:bidonvalue", subquery, slice++).then(function successCallback(response) {
+            if(response.data.length){
+              this.results[subquery].addAll(response.data);
+            } else {
+              stop = true;
+            }
+          }, function errorCallback() {
+            console.log("no data was retrieved");
+          });
+        }
+      }
+    }
+  }
+});
