9b016c95f5e31817ea08170e34750acc7b673729
Rintarou <merlin.barzilai@gmail.com>
2018-04-12 17:57:26 +0200

    simplified script, cause node does the work

---
+++ scripts/run_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=$1+".csv"
+echo "QueryNumber, LDFtime(ms), ZZtime(ms), soundness" > $FILE
+
+# while read -r; do
+#   line="$REPLY"
+#   echo "$REPLY"
+# done
+
+for FILENAME in queries/*.rq;
+do
+  node client.js $FILENAME >> $FILE
+done
+
+# node client.js queries/query_1017.rq >> $FILE
